f4233a46797939eb5a699032a87f72c5849efb15
arifamsar <dcylert@gmail.com>
2024-05-23 23:06:32 +0700

    Refactor coreteams.js and divisions.js, update team positions and members

--- src/data/divisions.js
+++ src/data/divisions.js
@@ -1,30 +1,37 @@
 export const divisions = [
   {
     id: 1,
-    name: "Kaderisasi",
+    name: "Humas",
+    members: ["M. Alfiqri Maulana", "Putri Sundari", "Said Lubawi", "Aldiansyah", "Muhammad Haqqi Syahputra", "Alfi Syahrin", "Anindya Nurul Fadillah"],
   },
   {
     id: 2,
-    name: "Hubungan Masyarakat",
+    name: "Kominfo",
+    members: ["Radhil Fauzi", "R. Citra Dwinanda Putri", "Muhammad Raihan Kausar", "Muhammad Rafi Rafta", "Wahyu Ramadhan. S", "M. Teguh Wijaksono", "Fadhlan Ardanel Razaq"],
   },
   {
     id: 3,
-    name: "Komunikasi & Informasi",
+    name: "Kerohanian",
+    members: ["Ana Raudhatul Jannah", "Rifki Nul Arif", "Wahyu Muliadi Siregar", "Agung Lexsi Kurniawan", "Mhd Hammam Almahdi"],
   },
   {
     id: 4,
     name: "Riset & Teknologi",
+    members: ["Rafsamjani Anugrah", "Muhammad Arif Amsar", "Randy Ivando", "Muhammad Darsik I'bmal", "Sulthon Abiyyiaflah", "Slamet Kurniawan", " Eghi Pratama"],
   },
   {
     id: 5,
-    name: "Dinas Pemuda & Olahraga",
+    name: "Kewirausahaan",
+    members: ["Abdul Haris", "Afdy Ilham Nopendra", "Mhd. Faisal Ariyoga", "Nurmia Bahar", "Salsabila Rahma Nst", "Alex Apriansa", "M. Hafizh Fadhillah"],
   },
   {
     id: 6,
-    name: "Kewirausahaan",
+    name: "Kaderisasi",
+    members: ["Febrian Sojuangon", "Namira Syalwa Alifa", "Giovanni Febian Panggabean", "Zakia Aulia Putra", "Jelita Lestari", "Muthia Sayyidah Syifa", "Muhammad Fauzan Fikri"],
   },
   {
     id: 7,
-    name: "Kerohanian",
-  }
+    name: "Pemuda dan Olahraga",
+    members: ["Wahyu Dwi Candra", "Sindi Maharani", "A. Musfiqurrahman Alfikri HN", "Dalil Arif Fadilah", "Zhorif Muflih", "Muflif Fadhlulrrochman", "Dody Saputra Simanjorang"],
+  },
 ];
