dfac54d29c361e12b03b79f1280cccecbfc66fb2
arifamsar <dcylert@gmail.com>
2024-05-23 12:38:46 +0700

    Update login functionality and add Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,37 @@
 import NavBar from "./components/NavBar";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Article from "./pages/Article";
 import Profile from "./pages/Profile";
+import Login from "./pages/Login";
 import Footer from "./components/Footer";
 import DetailArticle from "./pages/DetailArticle";
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext();
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = () => {
+    setIsAuthenticated(true);
+  }
+
+  const logout = () => {
+    setIsAuthenticated(false);
+  }
   return (
-    <>
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/article" element={<Article />} />
         <Route path="/article/:id" element={<DetailArticle />} />
+        <Route path="/login" element={isAuthenticated ? <Navigate to={"/"} /> : <Login />} />
       </Routes>
       <Footer />
-    </>
+    </AuthContext.Provider>
   );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,6 +12,7 @@ export default function NavBar() {
     window.addEventListener("resize", () => window.innerWidth >= 960 && setOpenNav(false));
   }, []);
 
+
   const navList = (
     <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
       <Typography as="li" color="blue-gray" className="relative">
@@ -59,7 +60,7 @@ export default function NavBar() {
             <div className="mr-4 hidden lg:block">{navList}</div>
             <div className="flex items-center gap-x-1">
               <Button color="green" buttonType="filled" size="regular" ripple="light" className="hidden lg:block">
-                Login
+                <Link to="/login">Login</Link>
               </Button>
             </div>
             <IconButton variant="text" className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden" ripple={false} onClick={() => setOpenNav(!openNav)}>
@@ -79,7 +80,7 @@ export default function NavBar() {
           {navList}
           <div className="flex items-center gap-x-1">
             <Button fullWidth variant="gradient" size="sm" className="" color="green" buttonType="filled">
-              <span>Sign in</span>
+              <Link to="/login">Login</Link>
             </Button>
           </div>
           </MobileNav>
