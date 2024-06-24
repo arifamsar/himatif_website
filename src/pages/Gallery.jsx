c6baa0e9444e6fde03e4dbde97592cbe7ea15a84
arifamsar <dcylert@gmail.com>
2024-06-24 17:35:32 +0700

    feat: Update Gallery page with heading and styling

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,7 +1,13 @@
+import { Typography, Input } from "@material-tailwind/react";
+
 const Gallery = () => {
   return (
-    <div>
-      <h1>Gallery</h1>
+    <div className="container px-4 py-8 mx-auto">
+      <Typography tag="h1" color="green" className="text-3xl font-bold mb-4 text-center">
+        Halaman Galeri
+      </Typography>
+
+      
     </div>
   );
 };
