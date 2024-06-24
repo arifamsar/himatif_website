8e069db42b46d5237c535ea684bb35f8219fe2f9
arifamsar <dcylert@gmail.com>
2024-06-24 11:56:41 +0700

    navbar: Remove login button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -68,11 +68,6 @@ export default function NavBar() {
           </div>
           <div className="flex items-center gap-4">
             <div className="mr-4 hidden lg:block">{navList}</div>
-            <div className="flex items-center gap-x-1">
-              <Button color="green" buttonType="filled" size="regular" ripple="light" className="hidden lg:block">
-                <Link to="/login">Login</Link>
-              </Button>
-            </div>
             <IconButton variant="text" className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden" ripple={false} onClick={() => setOpenNav(!openNav)}>
               {openNav ? (
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" className="h-6 w-6" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
@@ -89,9 +84,6 @@ export default function NavBar() {
         <MobileNav open={openNav}>
           {navList}
           <div className="flex items-center gap-x-1">
-            <Button fullWidth variant="gradient" size="sm" className="" color="green" buttonType="filled">
-              <Link to="/login">Login</Link>
-            </Button>
           </div>
           </MobileNav>
       </Navbar>
