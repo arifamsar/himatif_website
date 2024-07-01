50cae8c066a97367d197f5dda7294eaab4196c13
arifamsar <dcylert@gmail.com>
2024-07-01 14:02:46 +0700

    refactor: Update division descriptions in divisions.js

--- src/data/divisions.js
+++ src/data/divisions.js
@@ -2,36 +2,43 @@ export const divisions = [
   {
     id: 1,
     name: "Humas",
+    descriptions: "Divisi Humas merupakan divisi yang bertugas untuk mengelola informasi dan komunikasi antara pemerintah daerah dengan masyarakat.",
     members: ["M. Alfiqri Maulana", "Putri Sundari", "Said Lubawi", "Aldiansyah", "Muhammad Haqqi Syahputra", "Alfi Syahrin", "Anindya Nurul Fadillah"],
   },
   {
     id: 2,
     name: "Kominfo",
+    descriptions: "Divisi Kominfo merupakan divisi yang bertugas untuk mengelola informasi dan komunikasi antara pemerintah daerah dengan masyarakat.",
     members: ["Radhil Fauzi", "R. Citra Dwinanda Putri", "Muhammad Raihan Kausar", "Muhammad Rafi Rafta", "Wahyu Ramadhan. S", "M. Teguh Wijaksono", "Fadhlan Ardanel Razaq"],
   },
   {
     id: 3,
     name: "Kerohanian",
+    descriptions: "Divisi Kerohanian merupakan divisi yang bertugas untuk mengelola informasi dan komunikasi antara pemerintah daerah dengan masyarakat.",
     members: ["Ana Raudhatul Jannah", "Rifki Nul Arif", "Wahyu Muliadi Siregar", "Agung Lexsi Kurniawan", "Mhd Hammam Almahdi"],
   },
   {
     id: 4,
     name: "Riset & Teknologi",
+    descriptions: "Divisi Riset & Teknologi merupakan divisi yang bertugas untuk mengelola informasi dan komunikasi antara pemerintah daerah dengan masyarakat.",
     members: ["Rafsamjani Anugrah", "Muhammad Arif Amsar", "Randy Ivando", "Muhammad Darsik I'bmal", "Sulthon Abiyyiaflah", "Slamet Kurniawan", " Eghi Pratama"],
   },
   {
     id: 5,
     name: "Kewirausahaan",
+    descriptions: "Divisi Kewirausahaan merupakan divisi yang bertugas untuk mengelola informasi dan komunikasi antara pemerintah daerah dengan masyarakat.",
     members: ["Abdul Haris", "Afdy Ilham Nopendra", "Mhd. Faisal Ariyoga", "Nurmia Bahar", "Salsabila Rahma Nst", "Alex Apriansa", "M. Hafizh Fadhillah"],
   },
   {
     id: 6,
     name: "Kaderisasi",
+    descriptions: "Divisi Kaderisasi merupakan divisi yang bertugas untuk mengelola informasi dan komunikasi antara pemerintah daerah dengan masyarakat.",
     members: ["Febrian Sojuangon", "Namira Syalwa Alifa", "Giovanni Febian Panggabean", "Zakia Aulia Putra", "Jelita Lestari", "Muthia Sayyidah Syifa", "Muhammad Fauzan Fikri"],
   },
   {
     id: 7,
     name: "Pemuda dan Olahraga",
+    descriptions: "Divisi Pemuda dan Olahraga merupakan divisi yang bertugas untuk mengelola informasi dan komunikasi antara pemerintah daerah dengan masyarakat.",
     members: ["Wahyu Dwi Candra", "Sindi Maharani", "A. Musfiqurrahman Alfikri HN", "Dalil Arif Fadilah", "Zhorif Muflih", "Muflif Fadhlulrrochman", "Dody Saputra Simanjorang"],
   },
 ];
