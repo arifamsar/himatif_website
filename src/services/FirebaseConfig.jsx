4b6b74301d5b4e0a152478f25ab445d38ec28d8c
arifamsar <dcylert@gmail.com>
2024-07-04 09:52:38 +0700

    refactor: Remove console logs from FirebaseConfig.jsx

--- src/services/FirebaseConfig.jsx
+++ src/services/FirebaseConfig.jsx
@@ -21,6 +21,3 @@ const db = getFirestore(app);
 const analytics = getAnalytics(app);
 
 export { db, storage, analytics};
-
-console.log('API Key:', import.meta.env.VITE_FIREBASE_API_KEY);
-console.log('Project ID:', import.meta.env.VITE_FIREBASE_PROJECT_ID);
